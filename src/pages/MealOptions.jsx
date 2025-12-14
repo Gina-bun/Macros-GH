9144984c386edae762df3f1e4738682a12f0a79d
Gina-bun <64642337+Gina-bun@users.noreply.github.com>
2025-12-14 13:39:43 +0000

    feat: start meal display

--- src/pages/MealOptions.jsx
+++ src/pages/MealOptions.jsx
@@ -1,9 +1,11 @@
-import {useParams} from "react-router-dom"
+import {useNavigate, useParams} from "react-router-dom"
+import { Navigate } from "react-router-dom"
 import { ghanaMeals } from "../data/ghanaMeals"
 import { Navbar } from "../components/layout/Navbar"
 
 export function MealOptions(){
     const { mealType }  = useParams()
+    const returnHome = useNavigate()
 
     const filteredMeals = ghanaMeals.filter((meal) => 
         meal.tags.includes(mealType)
@@ -16,7 +18,8 @@ export function MealOptions(){
             {
                 filteredMeals.map((meal) => {
                     return <div key={meal.id}>
-                               <h3>{meal.name}</h3>
+                               <h3 >{meal.name}</h3>
+                               <p onClick={() => returnHome(-1)}>+</p>
                            </div>
                 })
             }
